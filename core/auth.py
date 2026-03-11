a018edebbc1f5c0c54d2b0cac5e238d675ef3375
Pedro Paulo Curto <ppcolopes@gmail.com>
2026-03-11 16:21:13 -0300

    Update auth.py

--- core/auth.py
+++ core/auth.py
@@ -9,21 +9,17 @@ class AuthManager:
         self.db.inicializar_tabelas()
 
     def verificar_login(self, usuario, senha):
-        """Valida usuário e senha de forma segura"""
         path = self.db._get_path('usuarios')
         df = pd.read_csv(path)
         
         if df.empty:
             return False
-            
-        # 1. Limpa os dados que o usuário digitou
+
         usuario_limpo = str(usuario).strip().lower()
         senha_hash = self.db.hash_password(senha)
-        
-        # 2. Limpa a coluna inteira do CSV usando .str (Correção do Erro!)
+
         df['usuario'] = df['usuario'].astype(str).str.strip().str.lower()
-        
-        # 3. Compara
+
         user_match = df[(df['usuario'] == usuario_limpo) & (df['senha'] == senha_hash)]
         
         if not user_match.empty:
@@ -33,17 +29,15 @@ class AuthManager:
         return False
 
     def cadastrar_usuario(self, usuario, senha):
-        """Cria um novo usuário se ele não existir"""
         path = self.db._get_path('usuarios')
         df = pd.read_csv(path)
         
         usuario = str(usuario).strip().lower()
-        
-        # Verifica se já existe
+
         if not df.empty:
             df['usuario'] = df['usuario'].astype(str).str.strip().str.lower()
             if usuario in df['usuario'].values:
-                return False, "Este nome de usuário já está em uso!"
+                return False, 
             
         novo_user = {
             'usuario': usuario,
@@ -52,7 +46,7 @@ class AuthManager:
         }
         
         self.db.salvar_registro('usuarios', novo_user)
-        return True, "Conta criada! Agora é só entrar na aba 'Entrar'."
+        return True, 
 
     def logout(self):
         st.session_state['autenticado'] = False
